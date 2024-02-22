153f8841be465ed8dafca419a588fe2b2a5bc139
Devops Training <saravana.devopstraining2023@gmail.com>
2024-02-22 11:06:19 +0530

    spring cloud gateway and circuit break examples

---
+++ labs/examples/spring-cloud-gateway-service/src/main/java/com/examples/scloud/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.examples.scloud;
+
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class GatewayConfig {
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("example", r -> r.path("/example/**")
+//                        .uri("http://localhost:8081")) // Forward requests to another service
+//                .build();
+//    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getRoute() {
+        return RouterFunctions.route().GET("/get", HandlerFunctions.http("https://httpbin.org")).build();
+    }
+}
